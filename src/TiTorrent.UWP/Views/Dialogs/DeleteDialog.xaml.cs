566e19e3e449840c326f756a499c5cc74a3d4a2c
Andrey <andrey.timofeev.and37@gmail.com>
2021-03-06 12:34:34 +0300

    Update 12:34

--- src/TiTorrent.UWP/Views/Dialogs/DeleteDialog.xaml.cs
+++ src/TiTorrent.UWP/Views/Dialogs/DeleteDialog.xaml.cs
@@ -1,14 +1,18 @@
-﻿using TiTorrent.UWP.Services;
+﻿using MonoTorrent.Client;
+using TiTorrent.UWP.Services;
 
 namespace TiTorrent.UWP.Views.Dialogs
 {
     public sealed partial class DeleteDialog
     {
-        public bool IsDeleteFiles { get; private set; }
-
-        public DeleteDialog()
+        public bool IsDeleteFiles { get; private set; } = true;
+        public TorrentManager Manager { get; }
+        
+        public DeleteDialog(TorrentManager manager)
         {
             RequestedTheme = ThemeSelectorService.Theme;
+            Manager = manager;
+
             InitializeComponent();
         }
     }
